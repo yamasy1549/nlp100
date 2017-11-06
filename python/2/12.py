27fb3800af5a75b2362e4dec3f7576e68dde6f61
yamasy1549 <sanae@yamasy.info>
2017-11-06 12:41:09 +0900

    出力調整

--- python/2/12.py
+++ python/2/12.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 filename = "hightemp.txt"
 df = pd.read_csv(filename, header=None, delimiter='\t')
-df.iloc[:,0].to_csv('col1.txt', index=False, header=False)
-df.iloc[:,1].to_csv('col2.txt', index=False, header=False)
+df.iloc[:,0].to_csv('../../output/col1.txt', index=False, header=False)
+df.iloc[:,1].to_csv('../../output/col2.txt', index=False, header=False)
--- python/2/16.py
+++ python/2/16.py
@@ -7,4 +7,4 @@ n = int(sys.argv[1])
 
 for i in range(n):
     chunk = lines[(l+1)*i//n : (l+1)*(i+1)//n]
-    open('hightemp16-{0}.txt'.format(i), 'w').write(''.join(chunk))
+    open('../../output/hightemp16-{0}.txt'.format(i), 'w').write(''.join(chunk))
