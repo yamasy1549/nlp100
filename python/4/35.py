d8ea2ed56e2dc960e420574751b0fdfebb3d3359
yamasy1549 <sanae@yamasy.info>
2017-12-03 22:08:29 +0900

    35誤読してた

--- python/4/35.py
+++ python/4/35.py
@@ -3,14 +3,15 @@ from functions4 import *
 long_nouns = []
 
 for sentence in morphological():
-    nouns = []
+    nouns = ""
 
     for word in sentence:
         if word["pos"] == "名詞":
-            nouns.append(word["surface"])
+            nouns += word["surface"]
         else:
-            if len(nouns) > len(long_nouns):
-                long_nouns = nouns
-            nouns = []
+            if len(nouns) > 1:
+                long_nouns.append(nouns)
+            nouns = ""
 
-print(''.join(long_nouns))
+for nouns in long_nouns:
+    print(nouns)
