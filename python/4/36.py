162f47c1b4e1bf40255eecd15cd79ab61d6010ce
yamasy1549 <sanae@yamasy.info>
2017-12-03 22:31:47 +0900

    同じランクのものが複数ある場合は文字コード順でソート

--- python/4/36.py
+++ python/4/36.py
@@ -2,5 +2,5 @@ from functions4 import *
 
 freqs = word_freq(morphological())
 
-for word, count in sorted(freqs.items(), key=lambda x:x[1], reverse=True):
+for word, count in sorted(freqs.items(), key=lambda x:(-x[1], x[0])):
     print(word, count)
