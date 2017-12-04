8c1ddc5665a9638d8a51513c67218308b370ef34
yamasy1549 <sanae@yamasy.info>
2017-12-04 09:15:17 +0900

    35誤読 文字の長さじゃなくて名詞の長さをみる

--- python/4/35.py
+++ python/4/35.py
@@ -3,15 +3,15 @@ from functions4 import *
 long_nouns = []
 
 for sentence in morphological():
-    nouns = ""
+    nouns = []
 
     for word in sentence:
         if word["pos"] == "名詞":
-            nouns += word["surface"]
+            nouns.append(word["surface"])
         else:
             if len(nouns) > 1:
                 long_nouns.append(nouns)
-            nouns = ""
+            nouns = []
 
 for nouns in long_nouns:
-    print(nouns)
+    print("".join(nouns))
