e409dbb1fc60e30f02e4c2eb842b76c13e413c6b
yamasy1549 <sanae@yamasy.info>
2017-12-04 15:15:35 +0900

    reverseを使わない

--- python/4/39.py
+++ python/4/39.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from functions4 import *
 
 freqs = word_freq(morphological())
-sorted_freqs = sorted(freqs.items(), key=lambda x:x[1], reverse=True)
+sorted_freqs = sorted(freqs.items(), key=lambda x:(-x[1], x[0]))
 
 x = [word[1] for word in sorted_freqs]
 plt.xscale('log')
