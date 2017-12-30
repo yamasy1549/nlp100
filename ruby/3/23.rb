2a986f4a5640e6d42c6d4214878b30259d705c6b
yamasy1549 <sanae@yamasy.info>
2017-12-30 09:11:57 +0900

    見出しの空白はいくつあってもいい気がしてきた

--- ruby/3/23.rb
+++ ruby/3/23.rb
@@ -2,6 +2,6 @@ require './util'
 
 article = gzip2hash["イギリス"]
 
-article.scan(/(?<level>={2,})\s?(?<name>.+?)\s?(?:\k<level>)/).each do |level, name|
+article.scan(/(?<level>={2,})\s*(?<name>.+?)\s*(?:\k<level>)/).each do |level, name|
   puts [name, level.length].join(" ")
 end
