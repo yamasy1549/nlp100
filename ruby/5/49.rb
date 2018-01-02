a5846b6f770bec1b9735dad73d56cc0d5cbeb2c3
yamasy1549 <sanae@yamasy.info>
2018-01-02 14:25:09 +0900

    49書き直し

--- ruby/5/49.rb
+++ ruby/5/49.rb
@@ -1,11 +1,11 @@
 require './util'
 
 dependencies.each do |sentence|
-  noun_pairs = sentence.select { |chunk| chunk.has_poss?("名詞") }.combination(2)
+  noun_pairs = sentence.select { |chunk| chunk.has_pos?("名詞") }.combination(2)
 
   noun_pairs.each do |i, j|
-    noun_i = i.morphs.find { |morph| morph.pos == "名詞" }
-    noun_j = j.morphs.find { |morph| morph.pos == "名詞" }
+    noun_i = i.first_morph("名詞")
+    noun_j = j.first_morph("名詞")
     surface_i = noun_i.surface
     surface_j = noun_j.surface
     noun_i.surface = "X"
@@ -14,8 +14,10 @@ dependencies.each do |sentence|
     path_i = i.path_to_root(sentence)
     path_j = j.path_to_root(sentence)
     if path_i.include?(j)
-      puts path_i[0..path_i.index(j)].map(&:surfaces).join(" -> ")
+      # 文節iから構文木の根に至る経路上に文節jが存在する場合
+      puts path_i[0...path_i.index(j)].map(&:surfaces).push("Y").join(" -> ")
     else
+      # 文節iと文節jから構文木の根に至る経路上で共通の文節kで交わる場合
       k = (path_i & path_j).first
       output_paths = [
         path_i[0...path_i.index(k)].map(&:surfaces).join(" -> "),
