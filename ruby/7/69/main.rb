8eac49f3f755a09e6bf9fccad7297d9bb71abcbf
yamasy1549 <sanae@yamasy.info>
2018-01-15 14:00:29 +0900

    69がパワーアップした :muscle:

--- ruby/7/69/main.rb
+++ ruby/7/69/main.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
 require 'mongo'
+require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 helpers do
@@ -9,17 +11,23 @@ helpers do
     return if hash.nil?
     [hash.dig('year'), hash.dig('month'), hash.dig('date')].compact.join('.')
   end
+
+  def display_count(count)
+    (count <= 1) ? "#{count} result:" : "#{count} results:"
+  end
 end
 
 before do
-  client = Mongo::Client.new(['127.0.0.1:27017'], database: '64')
-  @collection = client[:artists]
+  @client = Mongo::Client.new(['127.0.0.1:27017'], database: '64')
+  @collection = @client[:artists]
+  cache_control :public, :must_revalidate
 end
 
 get '/' do
   @name = params['name']&.strip
   @area = params['area']&.strip
   @tags = params['tags']&.split(/\s*,\s*/)
+  @limit = params['limit'].to_i == 0 ? 10 : params['limit'].to_i
 
   query = {}
   if @name.present?
@@ -31,7 +39,26 @@ get '/' do
   query['area'] = @area if @area.present?
   query['tags.value'] = { '$all': @tags } if @tags.present?
 
-  @artists = @collection.find(query).sort({ 'rating.value': -1 }).limit(100)
+  @artists = @collection.find(query).sort({ 'rating.value': -1 }).limit(@limit)
+
+  @artists = @artists.map do |artist|
+    url = "https://www.google.co.jp/search?"
+    params = {
+      q: "#{artist['name']} artist",
+      tbs: 'isch'
+    }
+
+    uri = URI.parse(url)
+    uri.query = URI.encode_www_form(params)
+
+    html = open(uri) do |f|
+      charset = f.charset
+      f.read
+    end
+    doc = Nokogiri::HTML.parse(html, nil, nil)
+    artist['image'] = doc.css('img').first&.attribute('src')&.value
+    artist
+  end
 
   erb :index
 end
